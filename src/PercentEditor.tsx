f4ffdb72eb8e25c435d4929f12a43a6e41312a1c
Jean-Baptiste WATENBERG <jeanbaptiste.watenberg@gmail.com>
2019-11-01 17:07:53 +0100

    fix(prettier)

--- src/PercentEditor.tsx
+++ src/PercentEditor.tsx
@@ -5,87 +5,89 @@ import { PercentPanelOptions } from './types';
 import { SelectableValue, Threshold } from '@grafana/data';
 
 export class PercentEditor extends PureComponent<PanelEditorProps<PercentPanelOptions>> {
-  onPercentOfChanged = ({value}: SelectableValue<string>) => {
+  onPercentOfChanged = ({ value }: SelectableValue<string>) => {
     this.props.onOptionsChange({ ...this.props.options, percentOf: value || '' });
   };
 
-  onOverChanged = ({value}: SelectableValue<string>) => {
+  onOverChanged = ({ value }: SelectableValue<string>) => {
     this.props.onOptionsChange({ ...this.props.options, over: value || '' });
   };
 
-  onDecimalChanged = ({value}: SelectableValue<number>) => {
+  onDecimalChanged = ({ value }: SelectableValue<number>) => {
     this.props.onOptionsChange({ ...this.props.options, decimal: value === undefined ? -1 : value });
-  }
-
-  onThresholdsCHanged = (newThresholds: Threshold[]) => {
-    this.props.onOptionsChange({ ...this.props.options, thresholds: newThresholds})
   };
 
-  onValueFontSizeChanged = ({value}: SelectableValue<string>) => {
+  onThresholdsCHanged = (newThresholds: Threshold[]) => {
+    this.props.onOptionsChange({ ...this.props.options, thresholds: newThresholds });
+  };
+
+  onValueFontSizeChanged = ({ value }: SelectableValue<string>) => {
     this.props.onOptionsChange({ ...this.props.options, valueFontSize: value || '' });
   };
 
-  onPercentFontSizeChanged = ({value}: SelectableValue<string>) => {
+  onPercentFontSizeChanged = ({ value }: SelectableValue<string>) => {
     this.props.onOptionsChange({ ...this.props.options, percentFontSize: value || '' });
   };
 
   render() {
     const { options, data } = this.props;
 
-    const fieldAliases: SelectableValue<string>[] = data.series.map((serie) => {
-      return serie.fields.filter(field => field.type === 'number').map((field) => {
-        return {label: field.name, value: field.name };
-      });
-    }).reduce((aggregated, current) => [...aggregated, ...current], []); 
+    const fieldAliases: SelectableValue<string>[] = data.series
+      .map(serie => {
+        return serie.fields
+          .filter(field => field.type === 'number')
+          .map(field => {
+            return { label: field.name, value: field.name };
+          });
+      })
+      .reduce((aggregated, current) => [...aggregated, ...current], []);
 
     const decimalOptions = [
-      {label: 'No limit', value: -1},
-      ...Array(7).fill(undefined).map((_: undefined, index: number) => ({label: `${index}`, value: index}))
-    ]
+      { label: 'No limit', value: -1 },
+      ...Array(7)
+        .fill(undefined)
+        .map((_: undefined, index: number) => ({ label: `${index}`, value: index })),
+    ];
 
-    const fontSizeOptions = [
-      20, 30, 50, 70, 80, 100, 110, 120, 150, 170, 200
-    ].map(size => ({label: `${size}%`, value: `${size}`}))
-    
-    return (<PanelOptionsGrid>
-      <div className="section gf-form-group">
-        <h5 className="section-heading">Percent of</h5>
-        <Select
-          value={{label: options.percentOf, value: options.percentOf }}
-          options={fieldAliases}
-          onChange={this.onPercentOfChanged}
-        /><br />
+    const fontSizeOptions = [20, 30, 50, 70, 80, 100, 110, 120, 150, 170, 200].map(size => ({ label: `${size}%`, value: `${size}` }));
 
-        <h5 className="section-heading">Over</h5>
-        <Select
-          value={{label: options.over, value: options.over }}
-          options={fieldAliases}
-          onChange={this.onOverChanged}
-        /><br />
+    return (
+      <PanelOptionsGrid>
+        <div className="section gf-form-group">
+          <h5 className="section-heading">Percent of</h5>
+          <Select value={{ label: options.percentOf, value: options.percentOf }} options={fieldAliases} onChange={this.onPercentOfChanged} />
+          <br />
 
-        <h5 className="section-heading">Decimals</h5>
-        <Select
-          value={decimalOptions.find(decimal => decimal.value === options.decimal)}
-          options={decimalOptions}
-          onChange={this.onDecimalChanged}
-        /><br />
+          <h5 className="section-heading">Over</h5>
+          <Select value={{ label: options.over, value: options.over }} options={fieldAliases} onChange={this.onOverChanged} />
+          <br />
 
-        <h5 className="section-heading">Value font size</h5>
-        <Select
-          value={fontSizeOptions.find(size => size.value === options.valueFontSize)}
-          options={fontSizeOptions}
-          onChange={this.onValueFontSizeChanged}
-        /><br />
+          <h5 className="section-heading">Decimals</h5>
+          <Select
+            value={decimalOptions.find(decimal => decimal.value === options.decimal)}
+            options={decimalOptions}
+            onChange={this.onDecimalChanged}
+          />
+          <br />
 
-        <h5 className="section-heading">% font size</h5>
-        <Select
-          value={fontSizeOptions.find(size => size.value === options.percentFontSize)}
-          options={fontSizeOptions}
-          onChange={this.onPercentFontSizeChanged}
-        />
-      </div>
+          <h5 className="section-heading">Value font size</h5>
+          <Select
+            value={fontSizeOptions.find(size => size.value === options.valueFontSize)}
+            options={fontSizeOptions}
+            onChange={this.onValueFontSizeChanged}
+          />
+          <br />
+
+          <h5 className="section-heading">% font size</h5>
+          <Select
+            value={fontSizeOptions.find(size => size.value === options.percentFontSize)}
+            options={fontSizeOptions}
+            onChange={this.onPercentFontSizeChanged}
+          />
+        </div>
 
         <ThresholdsEditor thresholds={options.thresholds} onChange={this.onThresholdsCHanged} />
-    </PanelOptionsGrid>);
+      </PanelOptionsGrid>
+    );
   }
 }
--- src/PercentPanel.tsx
+++ src/PercentPanel.tsx
@@ -2,13 +2,17 @@ import React, { PureComponent, PropsWithChildren } from 'react';
 import { PanelProps, getColorFromHexRgbOrName } from '@grafana/ui';
 import { PercentPanelOptions } from 'types';
 
-interface Props extends PanelProps<PercentPanelOptions> { }
+interface Props extends PanelProps<PercentPanelOptions> {}
 
 const BASE_FONT_SIZE = 38;
 
-function SpanValue({fontSizePercent, className, children}: PropsWithChildren<{fontSizePercent: string, className: string}>) {
+function SpanValue({ fontSizePercent, className, children }: PropsWithChildren<{ fontSizePercent: string; className: string }>) {
   const pixelSize = (parseInt(fontSizePercent, 10) / 100) * BASE_FONT_SIZE;
-  return <span className={className} style={{fontSize: pixelSize + 'px'}}>{children}</span>
+  return (
+    <span className={className} style={{ fontSize: pixelSize + 'px' }}>
+      {children}
+    </span>
+  );
 }
 
 export class PercentPanel extends PureComponent<Props> {
@@ -18,32 +22,38 @@ export class PercentPanel extends PureComponent<Props> {
     const percentOverSerie = data.series.find(serie => serie.fields.find(field => field.name === options.over));
 
     if (!percentOfSerie || !percentOverSerie) {
-      return <p>Selected series are not available</p>
+      return <p>Selected series are not available</p>;
     }
 
     const percentOfField = percentOfSerie.fields.find(field => field.name === options.percentOf);
     const percentOverField = percentOverSerie.fields.find(field => field.name === options.over);
 
     if (!percentOfField || !percentOverField) {
-      return <p>Selected fields are not available</p>
+      return <p>Selected fields are not available</p>;
     }
 
     const percentOfSum = percentOfField.values.toArray().reduce((sum, current) => sum + current, 0);
     const percentOverSum = percentOverField.values.toArray().reduce((sum, current) => sum + current, 0);
 
-    const percent = percentOfSum * 100 / percentOverSum
+    const percent = (percentOfSum * 100) / percentOverSum;
 
     const applyableThreshold = options.thresholds
       .sort((thresholdA, thresholdB) => thresholdB.value - thresholdA.value)
-      .find(threshold => threshold.value <= percent) || {color: 'transparent'};
-    
-    console.log(options.thresholds, applyableThreshold)
+      .find(threshold => threshold.value <= percent) || { color: 'transparent' };
 
-    return <div className="singlestat-panel" style={{background: getColorFromHexRgbOrName(applyableThreshold.color)}}>
-      <div className="singlestat-panel-value-container">
-        <SpanValue className="singlestat-panel-value" fontSizePercent={options.valueFontSize}>{options.decimal !== -1 ? percent.toFixed(options.decimal) : percent}</SpanValue>
-        <SpanValue className="singlestat-panel-postfix" fontSizePercent={options.percentFontSize}>%</SpanValue>
+    console.log(options.thresholds, applyableThreshold);
+
+    return (
+      <div className="singlestat-panel" style={{ background: getColorFromHexRgbOrName(applyableThreshold.color) }}>
+        <div className="singlestat-panel-value-container">
+          <SpanValue className="singlestat-panel-value" fontSizePercent={options.valueFontSize}>
+            {options.decimal !== -1 ? percent.toFixed(options.decimal) : percent}
+          </SpanValue>
+          <SpanValue className="singlestat-panel-postfix" fontSizePercent={options.percentFontSize}>
+            %
+          </SpanValue>
+        </div>
       </div>
-    </div>;
+    );
   }
 }
